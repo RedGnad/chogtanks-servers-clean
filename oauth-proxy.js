72b0ab2e446b0c6d031faed2346d35a88f1b663a
RedGnad <mookipstore@hotmail.com>
2025-09-03 23:23:56 +0200

    oauth-proxy

---
+++ oauth-proxy.js
@@ -0,0 +1,3 @@
+// Minimal bootstrap: starts the signature server from the subfolder.
+// Useful if the platform starts from the repo root.
+require('./chogtanks-servers-clean/signature-server.js');
